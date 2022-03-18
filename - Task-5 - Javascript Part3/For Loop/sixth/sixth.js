2e64e6b6dfce2d39601912b7b56f259a2e6ebca2
AmrYasserSaber <amr.yassr.2@gmail.com>
2022-03-18 21:56:42 +0200

    task

--- - Task-5 - Javascript Part3/For Loop/sixth/sixth.js
+++ - Task-5 - Javascript Part3/For Loop/sixth/sixth.js
@@ -1,12 +1,14 @@
 let start = 0;
 let swappedName = "elZerO";
-for (const i in swappedName) {
-    if (i.toUpperCase() === i) {
-        swappedName.replace(i,i.toLowerCase);
-      } else {
-        swappedName.replace(i,i.toUpperCase);
-      }
+var newWord = [];
+for (i=0; i<swappedName.length; i++) {
+  if (swappedName[i] == swappedName[i].toLowerCase()) {
+      newWord[i] = swappedName[i].toUpperCase();
+  }
+  else {
+    newWord[i] = swappedName[i].toLowerCase();
+  }
 }
-console.log(`"${swappedName}"`)
+console.log(newWord.join(""));
 // Output
-"ELzERo"
+//"ELzERo"
