2fe8f0c6ea66a83f7a0e249b835e41e6259a3eab
AmrYasserSaber <amr.yassr.2@gmail.com>
2022-04-20 22:53:05 +0200

    Merge branch 'master' of https://github.com/AmrYasserSaber/Web-training---IEEE-CS22-ZSB

---
+++ - Task-8 - JavaScript part6/Destructuring/third/third.js
@@ -0,0 +1,11 @@
+let arr1 = ["Ahmed", "Sameh", "Sayed"];
+let arr2 = ["Mohamed", "Gamal", "Amir"];
+let arr3 = ["Haytham", "Shady", "Mahmoud"];
+
+let[ ,a]=arr3
+let[ , ,b]=arr3
+let[c]=arr1
+
+console.log(`My Best Friends: ${a}, ${b}, ${c}`);
+
+// My Best Friends: Shady, Mahmoud, Ahmed
